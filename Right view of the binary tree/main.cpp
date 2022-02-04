9109beb0414ad370b4eb554f84ed525b1ffcd539
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-02-04 14:49:45 +0530

    04-02-2022

---
+++ Right view of the binary tree/main.cpp
@@ -0,0 +1,26 @@
+vector<int> rightView(Node *root)
+    {
+       vector<int> list;
+       queue<Node*> q;
+       if(root==NULL){
+           return list;
+       }
+       q.push(root);
+       while(q.empty()==false){
+           int n=q.size();
+           for(int i=1;i<=n;i++){
+               Node*temp=q.front();
+               q.pop();
+               if(i==1){
+                   list.push_back(temp->data);
+               }
+               if(temp->right!=NULL){
+                   q.push(temp->right);
+               }
+               if(temp->left!=NULL){
+                   q.push(temp->left);
+               }
+           }
+       }
+       return list;
+    }
