7198a06b71e04738db1242c10091dfae18eecab8
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-02-08 23:40:38 +0530

    08-02-2022

---
+++ Print path from root to a given node in a binary tree/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    int data;
+    Node*left;
+    Node*right;
+    Node(int num){
+        this->data=num;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+Node*addNode(int num){
+    Node*temp=new Node(num);
+    return temp;
+}
+bool hasPath(Node*root,vector<int> &v,int x){
+    if(root==NULL){
+        return false;
+    }
+    v.push_back(root->data);
+    if(root->data==x){
+        return true;
+    }
+    if(hasPath(root->left,v,x)||hasPath(root->right,v,x)){
+        return true;
+    }
+    v.pop_back();
+    return false;
+}
+void printPath(Node*root,int x){
+    if(root==NULL){
+        return;
+    }
+    vector<int> v;
+    if(hasPath(root,v,x)){
+        for(auto it=v.begin();it!=v.end();it++){
+            cout<< *it<<" ";
+        }
+    }
+    else{
+        cout<<"Path dosen't exists";
+    }
+}
+int main()
+{
+    Node*root=addNode(1);
+    root->left=addNode(2);
+    root->right=addNode(3);
+    root->left->left=addNode(6);
+    root->left->right=addNode(4);
+    root->left->right->left=addNode(5);
+    root->right->right=addNode(7);
+    printPath(root,5);
+    return 0;
+}
