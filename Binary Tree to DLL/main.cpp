aa056e16f327542db9159b3e1b4c4e9d8cf1ed4a
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-02-09 22:11:15 +0530

    09-02-2022

---
+++ Binary Tree to DLL/main.cpp
@@ -0,0 +1,40 @@
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+void findValues(Node*root,vector<int> &v){
+        if(root==NULL){
+            return;
+        }
+        findValues(root->left,v);
+        v.push_back(root->data);
+        findValues(root->right,v);
+    }
+    Node * bToDLL(Node *root)
+    {
+        if(root==NULL){
+            return nullptr;
+        }
+        Node*head=NULL,*tail=NULL,*temp;
+        vector<int> v;
+        findValues(root,v);
+        for(auto it=v.begin();it!=v.end();it++){
+            int num=*it;
+            temp=newNode(num);
+            if(head==NULL){
+                head=tail=temp;
+            }
+            else{
+                tail->right=temp;
+                temp->left=tail;
+                tail=temp;
+            }
+        }
+        return head;
+    }
