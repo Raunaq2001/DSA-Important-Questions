fb4dbe0903d37fb0778572297cfd6d2f0580d992
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-02-20 21:11:13 +0530

    20-02-22

---
+++ Selection Sort/main.cpp
@@ -0,0 +1,12 @@
+void selectionSort(int arr[],int n){
+    int temp;
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+}
