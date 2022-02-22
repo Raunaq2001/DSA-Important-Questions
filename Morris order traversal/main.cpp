b7020ee8168b6c4ecbed06132f254bf32c134322
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-02-22 22:52:08 +0530

    22-02-22

---
+++ Morris order traversal/main.cpp
@@ -0,0 +1,31 @@
+// @Author: RAUNAQ SINGH
+vector<int> inOrder(Node* root) {
+        vector<int> v;
+        if(root==NULL){
+            return v;
+        }
+        Node*curr=root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                v.push_back(curr->data);
+                curr=curr->right;
+            }
+            else{
+                Node*temp=curr->left;
+                while(temp->right!=NULL && temp->right!=curr){
+                    temp=temp->right;
+                }
+                if(temp->right==NULL){
+                    temp->right=curr;
+                    /*v.push_back(curr->data); If this line is written here then it will result in preorder traversal */
+                    curr=curr->left;
+                }
+                else{
+                    temp->right=NULL;
+                    v.push_back(curr->data); // If this line is written here then it will result in inorder traversal
+                    curr=curr->right;
+                }
+            }
+        }
+        return v;
+    }
