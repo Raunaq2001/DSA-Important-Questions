18c901e524bda743711246318a5369b6a5555d82
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-06-20 17:05:33 +0530

    Update main.cpp

--- Merge two sorted lists/main.cpp
+++ Merge two sorted lists/main.cpp
@@ -1,3 +1,4 @@
+// Reccursive approach
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode*result=NULL;
         if(list1==NULL){
@@ -15,4 +16,54 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
             result->next=mergeTwoLists(list1,list2->next);
         }
         return result;
+    }
+// Non reccursive approach
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL){
+            return list2;
+        }
+        if(list2==NULL){
+            return list1;
+        }
+        ListNode*head=NULL,*temp=NULL;
+        int i=1;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val<=list2->val){
+                if(i==1){
+                    head=temp=new ListNode(list1->val);
+                    i++;
+                }
+                else{
+                    temp->next=new ListNode(list1->val);
+                    temp=temp->next;
+                }
+                list1=list1->next;
+            }
+            else{
+                if(i==1){
+                    head=temp=new ListNode(list2->val);
+                    i++;
+                }
+                else{
+                    temp->next=new ListNode(list2->val);
+                    temp=temp->next;
+                }
+                list2=list2->next;
+            }
+        }
+        if(list1!=NULL){
+            while(list1!=NULL){
+                temp->next=new ListNode(list1->val);
+                temp=temp->next;
+                list1=list1->next;
+            }
+        }
+        if(list2!=NULL){
+            while(list2!=NULL){
+                temp->next=new ListNode(list2->val);
+                temp=temp->next;
+                list2=list2->next;
+            }
+        }
+        return head;
     }
