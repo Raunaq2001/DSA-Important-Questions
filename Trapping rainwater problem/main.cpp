3cb206d97a02dccaac66ff954cf2f56758895039
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-06-24 23:26:28 +0530

    Update main.cpp

--- Trapping rainwater problem/main.cpp
+++ Trapping rainwater problem/main.cpp
@@ -1,36 +1,17 @@
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-int calculateRainwater(int arr[],int n){
-    int left[n],right[n];
-    long long sum=0;
-    left[0]=arr[0];
-    for(int i=1;i<n-1;i++){
-        left[i]=max(left[i-1],arr[i]);
-    }
-    right[n-1]=arr[n-1];
-    for(int i=n-2;i>=0;i--){
-        right[i]=max(right[i+1],arr[i]);
-    }
-    for(int i=1;i<n-1;i++){
-        int val=min(left[i],right[i]);
-        if(val>arr[i]){
-            sum=sum+(val-arr[i]);
+// Leetcode question # 42
+int trap(vector<int>& height) {
+        int size=height.size(),ans=0;
+        vector<int> left(size),right(size);
+        left[0]=height[0];
+        right[size-1]=height[size-1];
+        for(int i=1;i<size;i++){
+            left[i]=max(left[i-1],height[i]);
         }
+        for(int i=size-2;i>=0;i--){
+            right[i]=max(right[i+1],height[i]);
+        }
+        for(int i=0;i<size;i++){
+            ans=ans+min(left[i],right[i])-height[i];
+        }
+        return ans;
     }
-    return sum;
-}
-int main()
-{
-    int arrSize;
-    cout<<"Enter the array size = ";
-    cin>>arrSize;
-    int arr[arrSize];
-    cout<<"Enter the array elements = ";
-    for(int i=0;i<arrSize;i++){
-        cin>>arr[i];
-    }
-    int result=calculateRainwater(arr,arrSize);
-    cout<<"Maximum rainwater trapped = "<<result;
-    return 0;
-}
