2d662f3c20fa29bae1064e0acbd66961224b0934
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-06-26 16:15:15 +0530

    2SUM

---
+++ 2SUM/main.cpp
@@ -0,0 +1,16 @@
+//Leetcode question # 1
+//@Author: RAUNAQ SINGH
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(i!=0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<nums.size();j++){
+                if(j!=i+1 && nums[j]==nums[j-1]) continue;
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                }
+            }
+        }
+        return ans;
+    }
