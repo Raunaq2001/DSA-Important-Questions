3f1f7d610ad0a629dcd4b0b77e35a22bf91e8d90
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-06-29 00:00:35 +0530

    29-06-2022

---
+++ Pow(x,n/main.cpp
@@ -0,0 +1,24 @@
+//Leetcode question # 50
+//Concept used: Binary Exponentiation
+//https://takeuforward.org/data-structure/implement-powxn-x-raised-to-the-power-n/
+    double myPow(double x, int n) {
+        double ans=1.0;
+        long long nn=n;
+        if(nn<0){
+            nn=nn*-1;
+        }
+        while(nn){
+            if(nn%2==0){
+                x=x*x;
+                nn=nn/2;
+            }
+            else{
+                ans=ans*x;
+                nn--;
+            }
+        }
+        if(n<0){
+            ans= (double)1.0/(double)ans;
+        }
+        return ans;
+    }
