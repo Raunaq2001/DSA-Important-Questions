70920e85bd0610b5a466ed2c21b46e0f640ef028
RAUNAQ SINGH <80464420+Raunaq2001@users.noreply.github.com>
2022-07-04 22:57:32 +0530

    Update main.cpp

--- Merge sort/main.cpp
+++ Merge sort/main.cpp
@@ -1,40 +1,50 @@
-void merging(int arr[],int lb,int mid,int ub){
-    int i=lb,j=mid+1,k=lb;
-    int copyArr[ub+1];
-    while(i<=mid && j<=ub){
-        if(arr[i]<arr[j]){
-            copyArr[k]=arr[i];
-            i++;
+//https://www.youtube.com/watch?v=4z9I6ZmeLOQ&ab_channel=ApnaCollege
+#include <iostream>
+using namespace std;
+void mergeSort(int arr[],int lb,int mid,int ub){
+    int n1=mid-lb+1;
+    int n2=ub-mid;
+    int a[n1],b[n2];
+    for(int i=0;i<n1;i++){
+        a[i]=arr[lb+i];
+    }
+    for(int i=0;i<n2;i++){
+        b[i]=arr[mid+i+1];
+    }
+    int i=0,j=0,k=lb;
+    while(i<n1 && j<n2){
+        if(a[i]<b[j]){
+            arr[k]=a[i];
+            i++;k++;
         }
         else{
-            copyArr[k]=arr[j];
-            j++;
-        }
-        k++;
-    }
-    if(i<=mid){
-        while(i<=mid){
-            copyArr[k]=arr[i];
-            i++;
-            k++;
+            arr[k]=b[j];
+            j++;k++;
         }
     }
-    else{
-        while(j<=ub){
-            copyArr[k]=arr[j];
-            j++;
-            k++;
-        }
+    while(i<n1){
+        arr[k]=a[i];
+        i++;k++;
     }
-    for(int i=lb;i<=ub;i++){
-        arr[i]=copyArr[i];
+    while(j<n2){
+        arr[k]=b[j];
+        j++;k++;
     }
 }
-void mergeSort(int arr,int lb,int ub){
+void merge(int arr[],int lb,int ub){
     if(lb<ub){
         int mid=(lb+ub)/2;
-        mergeSort(arr,lb,mid);
-        mergeSort(arr,mid+1,ub);
-        mergeing(arr,lb,mid,ub);
+        merge(arr,lb,mid);
+        merge(arr,mid+1,ub);
+        mergeSort(arr,lb,mid,ub);
     }
+}
+int main()
+{
+    int arr[]={34,4,13,65,87,34,89,1};
+    merge(arr,0,7);
+    for(int i=0;i<8;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
 }
