78a20eac15248c2b1b63cdd47db4db28d08db651
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-07-05 01:12:56 +0530

    05-07-2022

---
+++ Count and Say/main.cpp
@@ -0,0 +1,24 @@
+//Leetcode question # 38
+//https://www.youtube.com/watch?v=1YUqtoT9YoE&t=225s&ab_channel=CodeLibrary-byYogesh%26Shailesh
+    string countAndSay(int n) {
+        if(n==1) return "1";
+        if(n==2) return "11";
+        string s="11";
+        for(int i=3;i<=n;i++){
+            string t="";
+            s=s+'&';
+            int ctr=1;
+            for(int j=1;j<s.size();j++){
+                if(s[j]!=s[j-1]){
+                    t=t+to_string(ctr);
+                    t=t+s[j-1];
+                    ctr=1;
+                }
+                else{
+                    ctr++;
+                }
+            }
+            s=t;
+        }
+        return s;
+    }
