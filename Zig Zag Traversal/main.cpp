8b7b955c7af806194a9c3f2ebefaf9001296ec41
RAUNAQ SINGH <80464420+Raunaq2001@users.noreply.github.com>
2022-07-08 20:37:22 +0530

    Update main.cpp

--- Zig Zag Traversal/main.cpp
+++ Zig Zag Traversal/main.cpp
@@ -31,3 +31,31 @@ vector <int> zigZagTraversal(Node* root)
         }
         return ans;
     }
+
+
+
+//Leetcode question # 103
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if(root==NULL) return {};
+        vector<vector<int>> ans;
+        bool flag=false;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            vector<int> v;
+            for(int i=0;i<n;i++){
+                TreeNode * temp=q.front();
+                q.pop();
+                v.push_back(temp->val);
+                if(temp->left!=NULL) q.push(temp->left);
+                if(temp->right!=NULL) q.push(temp->right);
+            }
+            if(flag){
+                reverse(v.begin(),v.end());
+            }
+            flag=!flag;
+            ans.push_back({v});
+        }
+        return ans;
+    }
