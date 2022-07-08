2ab8b2bcd28e82776ce0c190f435f1f47ba515f7
RAUNAQ SINGH <80464420+Raunaq2001@users.noreply.github.com>
2022-07-08 15:08:04 +0530

    Update main.cpp

--- Check if binary tree is balanced or not/main.cpp
+++ Check if binary tree is balanced or not/main.cpp
@@ -4,6 +4,7 @@ int height(Node* root){
         }
         return max(height(root->left),height(root->right))+1;
     }
+//Reccursive approach
     bool isBalanced(Node *root)
     {
         if(root==NULL){
@@ -17,4 +18,29 @@ int height(Node* root){
         else{
             return false;
         }
+    }
+//Iterative Approach
+    //Leetcode question # 110
+    //Approach used: Level Order Traversal
+    //@Author: RAUNAQ SINGH
+    int height(TreeNode*root){
+        if(root==NULL){
+            return 0;
+        }
+        return max(height(root->left),height(root->right))+1;
+    }
+    bool isBalanced(TreeNode* root){
+        if(root==NULL) return false;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode*temp=q.front();
+            q.pop();
+            int lheight=height(temp->left);
+            int rheight=height(temp->right);
+            if(abs(lheight-rheight)>1) return false;
+            if(temp->left!=NULL) q.push(temp->left);
+            if(temp->right!=NULL) q.push(temp->right);
+        }
+        return true;
     }
