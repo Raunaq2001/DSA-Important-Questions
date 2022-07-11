9cbbe4434231a5d442340e8997dc051a11a18b39
RAUNAQ SINGH <80464420+Raunaq2001@users.noreply.github.com>
2022-07-11 22:16:38 +0530

    Update main.cpp

--- Diameter of a binary tree/main.cpp
+++ Diameter of a binary tree/main.cpp
@@ -15,3 +15,19 @@ int height(Node*root,int& ans){
         height(root,ans);
         return ans;
     }
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Leetcode question # 543
+//https://www.youtube.com/watch?v=Toe0UQMWhjM&ab_channel=AnujBhaiya
+    int height(TreeNode*root){
+        if(root==NULL) return 0;
+        int lheight=height(root->left);
+        int rheight=height(root->right);
+        return max(lheight,rheight)+1;
+    }
+    int diameterOfBinaryTree(TreeNode* root){
+        if(root==NULL) return 0;
+        int dl=diameterOfBinaryTree(root->left);
+        int dr=diameterOfBinaryTree(root->right);
+        int curr=height(root->left)+height(root->right);
+        return max(curr,max(dl,dr));
+    }
