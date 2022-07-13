04f76b2e7cf56c745e388ae0929583c0afed488c
Raunaq2001 <80464420+Raunaq2001@users.noreply.github.com>
2022-07-14 02:14:56 +0530

    14-07-2022

--- Kth largest element in BST/main.cpp
+++ Kth largest element in BST/main.cpp
@@ -1,34 +1,17 @@
-    // @Author: RAUNAQ SINGH
-    void findSize(Node*root,int &treeSize){
-        if(root==NULL){
-            return;
-        }
-        findSize(root->left,treeSize);
-        treeSize++;
-        findSize(root->right,treeSize);
+//@Author: RAUNAQ SINGH
+//Concept Used: Inorder Traversal
+//Pre-Requisite: K-th Smallest element in a BST
+    void getInorder(Node*root,vector<int> &v){
+        if(root==NULL) return;
+        getInorder(root->left,v);
+        v.push_back(root->data);
+        getInorder(root->right,v);
     }
-    void findAns(Node*root,int &counter,int target,int &ans){
-        if(root==NULL){
-            return;
-        }
-        findAns(root->left,counter,target,ans);
-        if(counter==target){
-            ans=root->data;
-        }
-        counter++;
-        findAns(root->right,counter,target,ans);
-    }
-    int kthLargest(Node *root, int k)
+    int kthLargest(Node *root, int K)
     {
-        if(root==NULL){
-            return -1;
-        }
-        int treeSize=0,counter=0,target,ans;
-        findSize(root,treeSize);
-        if(k>treeSize){
-            return -1;
-        }
-        target=treeSize-k;
-        findAns(root,counter,target,ans);
-        return ans;
+        if(root==NULL) return INT_MIN;
+        vector<int> v;
+        getInorder(root,v);
+        reverse(v.begin(),v.end()); //This line depends on which platform the code is being solved, as GFG consideres largest element from the last where as ideally it should have been from the begining.
+        return v[K-1];
     }
