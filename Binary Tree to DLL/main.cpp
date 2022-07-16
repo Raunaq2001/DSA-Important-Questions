be8d76372572c3ac61562afa6dc4a1dd98c82b95
RAUNAQ SINGH <80464420+Raunaq2001@users.noreply.github.com>
2022-07-16 10:09:05 +0530

    Update main.cpp

--- Binary Tree to DLL/main.cpp
+++ Binary Tree to DLL/main.cpp
@@ -38,3 +38,16 @@ void findValues(Node*root,vector<int> &v){
         }
         return head;
     }
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Leetcode question # 114
+//https://takeuforward.org/data-structure/flatten-binary-tree-to-linked-list
+    TreeNode*prev=NULL;
+    void flatten(TreeNode* root) {
+        if(root==NULL) return;
+        flatten(root->right);
+        flatten(root->left);
+        root->right=prev;
+        root->left=NULL;
+        prev=root;
+    }
