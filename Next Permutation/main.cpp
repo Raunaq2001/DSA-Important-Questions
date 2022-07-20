e3537c50c56f95a1b22067fbc7da5aeeb8aa21b5
RAUNAQ SINGH <80464420+Raunaq2001@users.noreply.github.com>
2022-07-21 01:03:02 +0530

    Update main.cpp

--- Next Permutation/main.cpp
+++ Next Permutation/main.cpp
@@ -1,24 +1,20 @@
-void nextPermutation(vector<int>& nums) {
-        int index=nums.size()-2,index1=-999,index2=0;
-        for(int i=index;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                index1=i;
-                break;
-            }
+//Algorithm: Starting from 2nd last index find a value that is less than it's next value once found that will be our index 1 then
+//           in another loop starting from last value till index+1 find a value that is greater than the element at index1 that 
+//           will be index2 then swap those values finally reverse the array from index+1 till end
+//https://takeuforward.org/data-structure/next_permutation-find-next-lexicographically-greater-permutation
+    void nextPermutation(vector<int>& nums){
+        int i,j;
+        for(i=nums.size()-2;i>=0;i--){
+            if(nums[i]<nums[i+1]) break;
         }
-        if(index1==-999){
+        if(i<0){
             reverse(nums.begin(),nums.end());
-
         }
         else{
-        index=nums.size()-1;
-        for(int i=index;i>index1;i--){
-            if(nums[i]>nums[index1]){
-                index2=i;
-                break;
+            for(j=nums.size()-1;j>i;j--){
+                if(nums[j]>nums[i]) break;
             }
-        }
-        swap(nums[index1],nums[index2]);
-        reverse(nums.begin()+index1+1,nums.end());
+            swap(nums[i],nums[j]);
+            reverse(nums.begin()+i+1,nums.end());
         }
     }
