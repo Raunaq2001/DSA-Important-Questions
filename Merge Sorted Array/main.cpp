e1f24aa35585e1c5202686012d39162292450825
RAUNAQ SINGH <80464420+Raunaq2001@users.noreply.github.com>
2022-07-21 11:37:40 +0530

    Update main.cpp

--- Merge Sorted Array/main.cpp
+++ Merge Sorted Array/main.cpp
@@ -39,3 +39,34 @@ void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
             }
         }
     }
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Approach 2
+    //@Author: RAUNAQ SINGH
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
+        if(n==0) return; //When n==0 do nothing simply return
+        //When m==0 delete all the 0 elements from nums1 and push all the elements from nums2
+        if(m==0){
+            int size=nums1.size();
+            while(size){
+                nums1.pop_back();
+                size--;
+            }
+            for(int i=0;i<n;i++){
+                nums1.push_back(nums2[i]);
+            }
+        }
+        else{
+            int itr=nums1.size()-n; //Find the number of non zero elements in nums1
+            for(int i=0;i<itr;i++){
+                if(nums1[i]>nums2[0]){
+                    swap(nums1[i],nums2[0]);
+                    sort(nums2.begin(),nums2.end());
+                }
+            }
+            int k=0;
+            for(int i=itr;i<nums1.size();i++){
+                nums1[i]=nums2[k];
+                k++;
+            }
+        }
+    }
